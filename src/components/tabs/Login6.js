453764ae53e1b6997b16f521e2e716fa3d2eb6e7
Chris Juber <cjuber5@gmail.com>
2020-08-05 14:46:44 -0600

    login 6 and updates to 1-5

--- src/components/tabs/Login6.js
+++ src/components/tabs/Login6.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react'
 export default class Login6 extends Component {
     render() {
         return (
-            <div>
-                Login6
+            
+            <div className='login6'>
+                <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'></link>
+               <div className='authBox'>
+                   <div className='toggle'>
+                       <h1>Sign In</h1>
+                       <h1 className='inactive'>Sign Up</h1>
+                   </div>
+                    <label>USERNAME</label>
+                    <input className='inputs' name='username' ></input>
+                    <label >PASSWORD</label>
+                    <input className='inputs' type='password' name='password' ></input>
+                    <button>Login</button>
+                </div>
             </div>
         )
     }
