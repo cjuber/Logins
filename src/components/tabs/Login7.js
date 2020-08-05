c4f930e63a517623a9f7cb7e283f06e69b8bab4a
Chris Juber <cjuber5@gmail.com>
2020-08-05 15:58:20 -0600

    added login 7 and updated index.html

--- src/components/tabs/Login7.js
+++ src/components/tabs/Login7.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export default class Login7 extends Component {
     render() {
         return (
-            <div>
-                Login7
-            </div>
+            <div className='login7'>
+            <div className='authBox'>
+                 
+                 <input className='inputs' name='username' placeholder='Username'></input>
+                 <input className='inputs' type='password' name='password' placeholder='Password'></input>
+                 <button>Submit</button>
+             </div>
+         </div>
         )
     }
 }
