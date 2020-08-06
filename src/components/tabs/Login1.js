d262b16ee40d89f68bddf093fc4f1d0176192969
Chris Juber <cjuber5@gmail.com>
2020-08-06 16:18:40 -0600

    updated Login 1 with sign up functionality and got rid of the circle

--- src/components/tabs/Login1.js
+++ src/components/tabs/Login1.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react'
 
 export default class Login1 extends Component {
+    constructor(){
+        super()
+        this.state={
+            toggle: false
+        }
+    }
+    toggleTrue = () =>{
+            this.setState({
+                toggle:true
+            })
+    }
+    toggleFalse = () =>{
+            this.setState({
+                toggle:false
+            })
+    }
     render() {
         return (
             <div className='login1'>
-                
+                <div className='tabs'>
+                        <button className={this.state.toggle ? ('tabButtonI') : ('tabButtonA')} onClick={this.toggleFalse}>Sign In</button>
+                        <button className={this.state.toggle ? ('tabButtonA') : ('tabButtonI')}onClick={this.toggleTrue}>Sign Up</button>
+
+                    </div>
                 <div className='authBox'>
-                    <div className='circle'></div>
+                    
+                    {/* <div className='circle'></div> */}
                    
                     
 
 
                     <div className='inputBox'>
-                        <input className='inputs' name='username' placeholder='Username'></input>
-                        <input className='inputs' type='password' name='password' placeholder='Password'></input>
+                        <input className={this.state.toggle ? ('inputNo') : ('inputs')} name='username' placeholder='Username'></input>
+                        <input className={this.state.toggle ? ('inputNo') : ('inputs')} type='password' name='password' placeholder='Password'></input>
+
+                        <input className={this.state.toggle ? ('inputs') : ('inputNo')} name='username' placeholder='Email'></input>
+                        <input className={this.state.toggle ? ('inputs') : ('inputNo')} name='username' placeholder='Username'></input>
+                        <input className={this.state.toggle ? ('inputs') : ('inputNo')} type='password' name='password' placeholder='Password'></input>
+                        
                         
                     </div>
 
 
-                    <button>Login</button>
+                    <button>{this.state.toggle ? ('Sign Up') : ('Sign In') }</button>
+                    <a href='#' className={this.state.toggle ? ('aT') : ('aF')}>Forgot Password</a>
+
+
+                    
                     
                 </div>
                 
