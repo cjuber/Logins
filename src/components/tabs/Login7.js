9dbb100cf11495147a92b85ea487b17f6d7a49ec
Chris Juber <cjuber5@gmail.com>
2020-08-07 10:50:44 -0600

    added sign up functionality to Login 7

--- src/components/tabs/Login7.js
+++ src/components/tabs/Login7.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
 
 export default class Login7 extends Component {
+    constructor(){
+        super()
+        this.state={
+            toggle: false
+        }
+    }
+    toggle = () =>{
+            this.setState({
+                toggle: !this.state.toggle
+            })
+    }
+    
+    
     render() {
         return (
             <div className='login7'>
             <div className='authBox'>
                  
-                 <input className='inputs' name='username' placeholder='Username'></input>
-                 <input className='inputs' type='password' name='password' placeholder='Password'></input>
-                 <button>Submit</button>
+                <input className={this.state.toggle ? ('inputNo') : ('inputs')}  name='username' placeholder='Username'></input>
+                <input className={this.state.toggle ? ('inputNo') : ('inputs')}  type='password' name='password' placeholder='Password'></input>
+
+                <input className={this.state.toggle ? ('inputs') : ('inputNo')} name='username' placeholder='Email'></input>
+                <input className={this.state.toggle ? ('inputs') : ('inputNo')} name='username' placeholder='Username'></input>
+                <input className={this.state.toggle ? ('inputs') : ('inputNo')} type='password' name='password' placeholder='Password'></input>
+                
+            <div>
+                <button>Submit</button>
+                <a href='#/7' className={this.state.toggle ? ('aT') : ('aF')}>Forgot Password</a>
+                <button onClick={this.toggle} className='toggleButton'>{this.state.toggle ? ('Sign Up') : ('Sign In')}</button>
+                
+            </div>
+            
              </div>
          </div>
         )
