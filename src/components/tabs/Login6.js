9f06cd75beb79f0ae5098163f411d8c23eff339a
Chris Juber <cjuber5@gmail.com>
2020-08-07 01:34:46 -0600

    added sign up functionality to Login 6

--- src/components/tabs/Login6.js
+++ src/components/tabs/Login6.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react'
 
 export default class Login6 extends Component {
+    constructor(){
+        super()
+        this.state={
+            toggle: false
+        }
+    }
+    toggleTrue = () =>{
+            this.setState({
+                toggle:true
+            })
+    }
+    toggleFalse = () =>{
+            this.setState({
+                toggle:false
+            })
+    }
     render() {
         return (
             
             <div className='login6'>
                 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'></link>
                <div className='authBox'>
-                   <div className='toggle'>
-                       <h1>Sign In</h1>
-                       <h1 className='inactive'>Sign Up</h1>
+                   <div className='tabs'>
+                   <button className={this.state.toggle ? ('tabButtonI') : ('tabButtonA')} onClick={this.toggleFalse}>Sign In</button>
+                    <button className={this.state.toggle ? ('tabButtonA') : ('tabButtonI')} onClick={this.toggleTrue}>Sign Up</button>
                    </div>
-                    <label>USERNAME</label>
-                    <input className='inputs' name='username' ></input>
-                    <label >PASSWORD</label>
-                    <input className='inputs' type='password' name='password' ></input>
-                    <button>Login</button>
-                    <a href='#/6'>Forgot Password?</a>
+                    <label className={this.state.toggle ? ('labelT') : ('labelF')}>USERNAME</label>
+                    <input className={this.state.toggle ? ('inputNo') : ('inputs')}  name='username' ></input>
+                    <label className={this.state.toggle ? ('labelT') : ('labelF')}>PASSWORD</label>
+                    <input className={this.state.toggle ? ('inputNo') : ('inputs')}  type='password' name='password' ></input>
+
+                    <label className={this.state.toggle ? ('labelF') : ('labelT')}>EMAIL</label>
+                    <input className={this.state.toggle ? ('inputs') : ('inputNo')}  name='username' ></input>
+                    <label className={this.state.toggle ? ('labelF') : ('labelT')}>USERNAME</label>
+                    <input className={this.state.toggle ? ('inputs') : ('inputNo')}  name='username'  ></input>
+                    <label className={this.state.toggle ? ('labelF') : ('labelT')}>PASSWORD</label>
+                    <input className={this.state.toggle ? ('inputs') : ('inputNo')}  type='password' name='password' ></input>
+
+                    <button>{this.state.toggle ? ('Sign Up') : ('Sign In')}</button>
+                    <a href='#' className={this.state.toggle ? ('aT') : ('aF')}>Forgot Password</a>
                 </div>
             </div>
         )
